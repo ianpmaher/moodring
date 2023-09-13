172b83b3ddb45ea21895bc33dbe5df8350558034
Ian Maher <ianpmaher@gmail.com>
2023-09-12 23:53:01 -0400

    set up routes for index show history new

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -11,9 +11,38 @@ const Log = require("../models/logs.js")
 // ============= //
 // INDEX ROUTE //
 router.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.render("index.ejs")
+})
+// ============= //
+// NEW ROUTE //
+router.get("/new", (req, res) => {
+  res.render("new.ejs")
+})
+// ============= //
+// HISTORY PAGE ROUTE //
+router.get("/history", async (req, res) => {
+  const foundLogs = await Log.find({})
+  res.render("logHistory.ejs", {
+    logs: foundLogs
+  })
+})
+// ============= //
+// SHOW ROUTE //
+router.get("/:id/", (req, res) => {
+  res.render("show.ejs")
 })
 
+// POST ROUTE "Create"
+router.post("/", async (req, res) => {
+  try {
+    const newLog = await Log.create(req.body)
+    console.log(newLog)
+    res.redirect("/logs")
+  } catch (err) {
+    console.log(err)
+    res.status(500).send(err)
+  }
+})
 
 // ============= //
 // EXPORTS //
--- models/logs.js
+++ models/logs.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose")
 // Schema constructor definitions
 const logSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  date: { type: Date, required: true },
-  time: { type: Date, required: true },
+  date: { type: String, required: true },
+  time: { type: String },
   mood: { type: Number, required: true, min: 1, max: 5, enum: [1,2,3,4,5]}, // maybe have enum? https://mongoosejs.com/docs/schematypes.html#numbers
-  emoji: { type: String },
-  tags: { type: [String] } // this should have tags as an array of strings
+  description: { type: String },
+  tags: { type: [String] }, // this should have tags as an array of strings
+  postedAt: { type: Date }
 })
 
 // create the Log model
